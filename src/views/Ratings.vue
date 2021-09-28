c1a4f92c8be07b143b487b58771275d31870223b
Wispik <wispik71@yandex.ru>
2021-09-28 18:45:34 +0300

    update

--- src/views/Ratings.vue
+++ src/views/Ratings.vue
@@ -14,10 +14,18 @@
       </div>
       <div class="rating-params">
           <app-select 
+            :items="constructor_select_items"
+            v-model="constructor_select_model"
+            default=1
+            width=288
+            v-if="tabs_active && tabs_active.id==8"
+          />
+          <app-select 
             :items="select_items"
             v-model="select_model"
             default=5
             width=288
+            v-else
           />
           <range-calendar 
             v-model='calendar'
@@ -25,6 +33,51 @@
           <button class="btn-blue btn-get-rating" @click="analyze">
               Построить рейтинг
           </button>
+      </div>
+      <div class="rating-constructor" v-if="tabs_active && tabs_active.id==8">
+          <div 
+            class="rating-constructor__item"
+            v-for="item in constructor"
+            :key="item.id"
+          >
+            <app-select 
+                :items="select_items"
+                v-model="item.select_model"
+                width=288
+            />
+            <label class="rating-constructor__item-label">
+                От
+                <input 
+                    type="text" 
+                    class="rating-constructor__item-input"
+                    placeholder="100"
+                    v-model="item.input_from"    
+                >
+            </label>
+            <label class="rating-constructor__item-label">
+                До
+                <input 
+                    type="text" 
+                    class="rating-constructor__item-input"
+                    placeholder="500"
+                    v-model="item.input_to"    
+                >
+            </label>
+            <img 
+                class="rating-constructor__button-x"
+                :src="require(`@/assets/images/icons/red circle_close.png`)"
+                @click="deleteItemFromConstructor(item.id)"
+                v-if="item.id != 1"
+            >
+          </div>
+      </div>
+      <div 
+        class="rating-constructor__button" 
+        @click="addItemToConstructor"
+        v-if="tabs_active && tabs_active.id==8"
+      >
+          <img :src="require(`@/assets/images/icons/add_more.png`)">
+          Добавить условие
       </div>
       <div class="ratings-table-block" v-show="show_table && tabs_active==tabs[0]">
           <div class="ratings-table__title">
@@ -109,6 +162,10 @@ export default {
                 {
                     id: 7,
                     title: 'Падения'
+                },
+                {
+                    id: 8,
+                    title: 'Конструктор'
                 }
             ],
             tabs_active: null,
@@ -159,7 +216,34 @@ export default {
             table_data: {},
             show_table_settings_collumns: false,
             show_table_settings_filters: false,
-            show_table: false
+            show_table: false,
+            constructor_select_items: [
+                {
+                    id: 1,
+                    title: 'Категории'
+                },
+                {
+                    id: 2,
+                    title: 'Бренды'
+                },
+                {
+                    id: 3,
+                    title: 'Продавцы'
+                },
+                {
+                    id: 4,
+                    title: 'Товары'
+                }
+            ],
+            constructor_select_model: null,
+            constructor: [
+                {
+                    id: 1,
+                    select_model: null,
+                    input_from: '',
+                    input_to: ''
+                }
+            ]
         }
     },
     mounted() {
@@ -168,6 +252,20 @@ export default {
     methods: {
         analyze() {
             this.show_table = true
+        },
+        addItemToConstructor() {
+            this.constructor.push(
+                {
+                    id: this.constructor[this.constructor.length-1].id + 1,
+                    select_model: null,
+                    input_from: '',
+                    input_to: ''
+                }
+            )
+        },
+        deleteItemFromConstructor(id) {
+            let res = this.constructor.filter(item => item.id != id)
+            this.constructor = [...res]
         }
     },
     components: {
@@ -216,5 +314,65 @@ export default {
         display: flex;
         flex-flow: row nowrap;
         gap: 8px;
+    }
+
+    .rating-constructor {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .rating-constructor__item {
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 20px;
+        align-items: center;
+        margin-top: 20px;
+    }
+
+    .rating-constructor__item-label {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        gap: 6px;
+        font-weight: 500;
+        font-size: 1.4rem;
+        line-height: 2.2rem;
+        letter-spacing: -0.01em;
+        color: rgba(0, 0, 0, 0.9);
+    }
+
+    .rating-constructor__item-input {
+        width: 152px;
+        padding: 8px 12px;
+        border: 1px solid #D9D9D9;
+        border-radius: 4px;
+        color: rgba(0, 0, 0, 0.9);
+        font-size: 1.4rem;
+        transition: 0.4s;
+
+        &:focus {
+            border: 1px solid #316D92
+        }
+    }
+
+    .rating-constructor__button-x {
+        cursor: pointer;
+    }
+
+    .rating-constructor__button {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        color: #316D92;
+        font-size: 1.4rem;
+        cursor: pointer;
+        gap: 4px;
+        margin-top: 20px;
+        margin-left: 60px;
+
+        & > img {
+            width: 32px;
+            height: 32px;
+        }
     }
 </style>
